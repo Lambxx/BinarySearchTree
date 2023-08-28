ac4da4fdc5fdf76e10e283fa7df7d93fee80e112
xoxolunaxox <23667944@stu.mmu.ac.uk>
2023-08-28 23:30:34 +0100

    Add project files.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class BinarySearchTree
+    {
+        private treeNode rootNode;
+
+        public void BinarySearchTree()
+        {
+
+        }
+
+    }
+    class treeNode
+    {
+        private treeNode leftNode;
+        private treeNode rightNode;
+        private int data;
+
+    }
+
+}
